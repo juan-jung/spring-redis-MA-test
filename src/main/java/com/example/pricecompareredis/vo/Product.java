1172864a30b7241cbbc70a9dc02d6ce48ed8555e
juan <crumbled23@gmail.com>
2024-07-09 20:38:23 +0900

    Fix : product vo  prodGrpId field added

--- src/main/java/com/example/pricecompareredis/vo/Product.java
+++ src/main/java/com/example/pricecompareredis/vo/Product.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class Product {
-
+    private String  prodGrpId;  // FPG0001
     private String productId; // 7fadd0bf-2ff5-488f-bcac-cf5af5452c26
     private int price; //25000
 }
